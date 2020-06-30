61c7d7682bae09c47220051cc1f38287d2d5c4fc
Shlomy Tourjeman <shlomy@aritmix.com>
2020-06-30 13:11:29 +0300

    minor modification

---
+++ LoggingExample/LoggingExample/Controllers/LogController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogInformation("Im here!");
+           
+            using (_logger.BeginScope(("correlation_id", Guid.NewGuid().ToString())))
+            {
+                _logger.LogInformation("Entering values/Get");
+
+                _logger.LogInformation("Log #2");
+                _logger.LogInformation("Large json is comming ahead");
+
+                _logger.LogInformation(
+              "{'host': 'example.org','short_message': 'A short message that helps you identify what is going on',  'full_message': 'Backtrace here more stuff', '_user_id': 9001,  '_some_info': 'foo',  '_some_env_var': 'bar'}"
+              );
+
+                _logger.LogInformation("Large json has been and gone");
+
+                _logger.LogInformation("Exit values/Get");
+            }
+            _logger.LogDebug("Lonely log here");
+
+            return Ok();
+        }
+    }
+}
---
+++ LoggingExample/LoggingExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using Gelf.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+
+namespace LoggingExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+                .CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .ConfigureLogging((context, builder) => builder.AddGelf(options =>
+                        {
+                            // Optional customisation applied on top of settings in Logging:GELF configuration section.
+                            options.LogSource = context.HostingEnvironment.ApplicationName;
+                            options.AdditionalFields["machine_name"] = Environment.MachineName;
+                            options.AdditionalFields["app_version"] = Assembly.GetEntryAssembly()
+                                ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+                        }));
+                })  ;
+
+    }
+}
+
+//    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+//        WebHost.CreateDefaultBuilder(args)
+//        .UseStartup<Startup>()
+//        .ConfigureLogging(loggingConfiguration => loggingConfiguration.ClearProviders())
+//        .UseSerilog((hostingContext, loggerConfiguration) =>
+//            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+//}
